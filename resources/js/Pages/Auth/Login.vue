b0f4bb674cc264aea88782402a3c61cc4c15fc59
Aenun Akhkam <aenunakhkam@gmail.com>
2026-06-27 21:52:19 +0700

    Update version text to 1.1.0 in Login.vue

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -75,7 +75,7 @@ const submit = () => {
             <p class="text-[15px] text-gray-500 font-medium mb-3">Masuk untuk mengakses <i class="font-bold">Layanan MiniBank App</i></p>
             <div class="inline-flex items-center gap-1.5 px-3 py-1 bg-[#0f7632]/10 border border-[#0f7632]/20 text-[#0f7632] text-xs font-bold rounded-lg shadow-sm">
                 <svg class="w-3.5 h-3.5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"></path></svg>
-                Versi 1.0
+                Versi 1.1.0
             </div>
         </div>
 
