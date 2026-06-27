15fa769fb4309b0de46a92f1d79e9f911d6b5f18
Aenun Akhkam <aenunakhkam@gmail.com>
2026-06-27 21:43:30 +0700

    Change 'Git Pull terakhir' to 'Update terakhir' in Updates.vue

--- resources/js/Pages/Updates.vue
+++ resources/js/Pages/Updates.vue
@@ -61,7 +61,7 @@ watch(perPage, (value) => {
                     </h3>
                     <div class="mt-4 md:mt-0 text-sm text-gray-400 font-medium flex items-center">
                         <svg class="w-4 h-4 mr-1.5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                        Git Pull terakhir: {{ lastFetch }}
+                        Update terakhir: {{ lastFetch }}
                     </div>
                 </div>
 
